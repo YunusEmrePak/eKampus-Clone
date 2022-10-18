1033b96241fa6d890874523d7f01f169dce819a3
Yunus Emre Pak <yunusemrepak@windowslive.com>
2022-10-18 16:23:50 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const main = document.querySelector(".main");
+const leftArrow = document.querySelector(".leftArrow");
+const rightArrow = document.querySelector(".rightArrow");
+const titleMain = document.querySelector(".titleMain");
+const contentMain = document.querySelector(".contentMain");
+
+arrayFoto = ["../img/mainfoto.png", "../img/mainfoto2.png"];
+var i = 0;
+
+leftClick = () => {
+    if (i == 0) {
+        main.style.backgroundImage = `url('${arrayFoto[1]}')`;
+        i++;
+        titleMain.innerHTML = "Unlimited Access to Information"
+        contentMain.innerHTML = "Electronic Databases, Books, Journals, Open Access Resources, Open Course Materials";
+    }
+    else {
+        main.style.backgroundImage = `url('${arrayFoto[0]}')`;
+        i--;
+        titleMain.innerHTML = "The Faculty of Open and Distance Education";
+        contentMain.innerHTML = "Innovation, quality, flexibilty, interaction, experience";
+    }
+}
+
+rightClick = () => {
+    if (i == 0) {
+        main.style.backgroundImage = `url('${arrayFoto[1]}')`;
+        i++;
+        titleMain.innerHTML = "Unlimited Access to Information"
+        contentMain.innerHTML = "Electronic Databases, Books, Journals, Open Access Resources, Open Course Materials";
+    }
+    else {
+        main.style.backgroundImage = `url('${arrayFoto[0]}')`;
+        i--;
+        titleMain.innerHTML = "The Faculty of Open and Distance Education";
+        contentMain.innerHTML = "Innovation, quality, flexibilty, interaction, experience";
+    }
+}
+
+leftArrow.addEventListener("click", leftClick);
+rightArrow.addEventListener("click", rightClick);
